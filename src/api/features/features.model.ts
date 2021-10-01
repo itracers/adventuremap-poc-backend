e3f0f9e2a9fbcb5df59ad689016601d0d83c64d4
cybor97 <muhametshin.1997@gmail.com>
2021-10-02 00:57:07 +0300

    Add minValue & maxValue for features

--- src/api/features/features.model.ts
+++ src/api/features/features.model.ts
@@ -1,4 +1,6 @@
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
+import { DataTypes } from 'sequelize';
+import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript';
+import { CountryFeatures } from '../countryFeatures/countryFeatures.model';
 
 @Table({ tableName: 'features', underscored: true, createdAt: 'created_at', updatedAt: 'updated_at' })
 export class Feature extends Model {
@@ -13,4 +15,12 @@ export class Feature extends Model {
 
   @Column
   type: string;
+
+  @HasMany(() => CountryFeatures)
+  countryFeatures: Array<CountryFeatures>
+
+  @Column(DataTypes.VIRTUAL)
+  minValue: string;
+  @Column(DataTypes.VIRTUAL)
+  maxValue: string;
 }
--- src/api/features/features.service.ts
+++ src/api/features/features.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize';
+import { CountryFeatures } from '../countryFeatures/countryFeatures.model';
 import { Feature } from './features.model';
 
 @Injectable()
@@ -10,7 +12,35 @@ export class FeaturesService {
   ) { }
 
   async findAll(): Promise<Feature[]> {
-    return this.featuresModel.findAll<Feature>();
+    return (await this.featuresModel.findAll<Feature>({
+      include: {
+        attributes: [
+          [Sequelize.fn('min', Sequelize.col('value')), 'minValueRaw'],
+          [Sequelize.fn('max', Sequelize.col('value')), 'maxValueRaw'],
+          [Sequelize.literal('min(cast(`value` as unsigned))'), 'minValueNumeric'],
+          [Sequelize.literal('max(cast(`value` as unsigned))'), 'maxValueNumeric']
+        ],
+        model: CountryFeatures
+      },
+      group: ['id']
+    }))
+      .map(feature => {
+        //@ts-ignore
+        if (feature.dataValues.countryFeatures[0]) {
+          //@ts-ignore
+          feature.minValue = feature.dataValues.countryFeatures[0].dataValues[feature.type === 'int' ? 'minValueNumeric' : 'minValueRaw'];
+          //@ts-ignore
+          feature.maxValue = feature.dataValues.countryFeatures[0].dataValues[feature.type === 'int' ? 'maxValueNumeric' : 'maxValueRaw'];
+        }
+        else {
+          feature.minValue = null;
+          feature.maxValue = null;
+        }
+        //@ts-ignore
+        delete feature.dataValues.countryFeatures;
+
+        return feature;
+      });
   }
 
   async findByPk(id: string | number): Promise<Feature> {
